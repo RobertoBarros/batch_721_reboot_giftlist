cf622e57104d2cf2b165ef9c09a03e767f15531a
Roberto Barros <roberto.f.barros@gmail.com>
2021-09-21 17:43:17 -0300

    Comentários

--- interface.rb
+++ interface.rb
@@ -2,12 +2,10 @@ require 'csv'
 require 'open-uri'
 require 'nokogiri'
 
+gifts = [] # gifts é um array de hashs
 
-# Welcome message
-puts 'Bem vindo, gaste seu dinheiro no reboot do Roberto'
-# Show actions / Ask action
-gifts = []
-
+# Storing em Arquivo CSV
+# (SOMENTE PARA AÇÕES QUE ALTERAM O ARRAY)
 def save(gifts)
   CSV.open('gifts.csv','wb') do |csv|
     gifts.each do |gift|
@@ -16,6 +14,8 @@ def save(gifts)
   end
 end
 
+# Parsing do Arquivo CSV
+# (SOMENTE AO INICIAR O PROGRAMA)
 def load(gifts)
   CSV.foreach('gifts.csv') do |row|
     name = row[0]
@@ -26,7 +26,7 @@ def load(gifts)
   end
 end
 
-
+# LISTAR TODOS OS PRODUTOS DO ARRAY
 def list(gifts)
   puts 'listar os itens'
   # Show gifts with index
@@ -37,6 +37,7 @@ def list(gifts)
   end
 end
 
+# ADICIONAR UM PRODUTO AO ARRAY
 def add(gifts)
   puts 'Que item quer adicionar'
   name = gets.chomp
@@ -49,6 +50,7 @@ def add(gifts)
   gifts << { name: name, price: price, bought: false }
 end
 
+# REMOVER UM PRODUTO DO ARRAY
 def delete(gifts)
   # Listar os items
   list(gifts)
@@ -60,6 +62,7 @@ def delete(gifts)
   gifts.delete_at(index)
 end
 
+# MARCAR UM PRODUTO COMO COMPRADO
 def mark(gifts)
   # List gifts
   list(gifts)
@@ -72,6 +75,7 @@ def mark(gifts)
   gifts[index][:bought] = true
 end
 
+# SCRAPE DA AMAZON.COM.BR
 def scrape(gifts)
   puts "Qual o produto para pesquisar?"
   product = gets.chomp
@@ -81,17 +85,17 @@ def scrape(gifts)
   html_text = URI.open(url).read
   html = Nokogiri::HTML(html_text)
 
-
-  products = []
+  products = [] # Array de Hash com o nome e proço do produto da amazom
   html.search('.s-result-item').first(10).each do |card|
     name = card.search('h2').text.strip
     price = card.search('.a-price-whole').text.strip.delete('.').delete(',').to_i
 
-    next if name == '' || price == ''
+    next if name == '' || price == '' # Ignora produtos que não possuem nome ou preço
 
     products << { name: name, price: price }
   end
 
+  # Mostra os produtos ao usuário
   puts "Escolha qual produto importar:"
   products.each_with_index do |product, index|
     puts "#{index + 1} #{product[:name]} R$ #{product[:price]}"
@@ -100,15 +104,26 @@ def scrape(gifts)
   puts "Entre com o número do produto:"
   index = gets.chomp.to_i - 1
 
+  # Adiciona o produto selecionado no array de gifts
   gifts << { name: products[index][:name], price: products[index][:price], bought: false }
 end
 
-load(gifts)
 
-loop do
+#########################################
+# NOSSO PROGRAMA INICIA A PARTIR DAQUI!
+#########################################
+
+# Welcome message
+puts 'Bem vindo, gaste seu dinheiro no reboot do Roberto'
+# Show actions / Ask action
+
+load(gifts) # Carrega os gifts do arquivo gifts.csv
+
+loop do # Loop infito para perguntar o que o usuário quer fazer
   puts 'O que deseja fazer [list|add|mark|delete|scrape|quit]?'
   action = gets.chomp.downcase
-# Execute action
+
+  # Execute action
   case action
   when 'list'
     list(gifts)
@@ -130,5 +145,6 @@ loop do
     puts 'Ação não conhecida, veja o menu!!!!'
   end
 end
+
 # Exit / Goodbye Message
 puts 'see you!'
