631afb10051830acd70c1a787087eb1d4787a84f
Daniel Perini <dsperini@gmail.com>
2021-09-21 12:30:20 -0300

    implementação dos métodos add e  list

--- interface.rb
+++ interface.rb
@@ -1,15 +1,28 @@
 # Welcome message
 puts 'Bem vindo, gaste seu dinheiro no reboot do Roberto'
 # Show actions / Ask action
+gifts = []
+
+def list(gifts)
+  puts 'listar os itens'
+  puts gifts
+end
+
+def add(gifts)
+  puts 'Que item quer adicionar'
+  item_add = gets.chomp
+  gifts << item_add
+end
+
 loop do
   puts 'O que deseja fazer [list|add|delete|quit]?'
   action = gets.chomp.downcase
 # Execute action
   case action
   when 'list'
-    puts 'listar os itens'
+    list(gifts)
   when 'add'
-    puts 'Que item quer adicionar'
+    add(gifts)
   when 'delete'
     puts 'Que item quer apagar'
   when 'quit'
