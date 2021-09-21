44a0f4d56920445b5849df1485557f621c26a2c6
Roberto Barros <roberto.f.barros@gmail.com>
2021-09-21 17:23:09 -0300

    implementa o scrape da Amazon

--- interface.rb
+++ interface.rb
@@ -1,4 +1,7 @@
 require 'csv'
+require 'open-uri'
+require 'nokogiri'
+
 
 # Welcome message
 puts 'Bem vindo, gaste seu dinheiro no reboot do Roberto'
@@ -64,15 +67,46 @@ def mark(gifts)
   # Ask gift index to mark as bought
   puts 'Qual marcar como comprado?'
   index = gets.chomp.to_i - 1
-  
+
   # Mark as bought!
   gifts[index][:bought] = true
+end
+
+def scrape(gifts)
+  puts "Qual o produto para pesquisar?"
+  product = gets.chomp
+
+  url = "https://www.amazon.com.br/s?k=#{product}"
+
+  html_text = URI.open(url).read
+  html = Nokogiri::HTML(html_text)
+
+
+  products = []
+  html.search('.s-result-item').first(10).each do |card|
+    name = card.search('h2').text.strip
+    price = card.search('.a-price-whole').text.strip.delete('.').delete(',').to_i
+
+    next if name == '' || price == ''
+
+    products << { name: name, price: price }
+  end
+
+  puts "Escolha qual produto importar:"
+  products.each_with_index do |product, index|
+    puts "#{index + 1} #{product[:name]} R$ #{product[:price]}"
+  end
+
+  puts "Entre com o número do produto:"
+  index = gets.chomp.to_i - 1
+
+  gifts << { name: products[index][:name], price: products[index][:price], bought: false }
 end
 
 load(gifts)
 
 loop do
-  puts 'O que deseja fazer [list|add|mark|delete|quit]?'
+  puts 'O que deseja fazer [list|add|mark|delete|scrape|quit]?'
   action = gets.chomp.downcase
 # Execute action
   case action
@@ -86,6 +120,9 @@ loop do
     save(gifts)
   when 'mark'
     mark(gifts)
+    save(gifts)
+  when 'scrape'
+    scrape(gifts)
     save(gifts)
   when 'quit'
     break
